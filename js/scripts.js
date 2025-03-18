ab4c82748029f5f5a89554cbc0297f5b93148fab
MkSeven2 <sarsembayevdias7@gmail.com>
2025-03-17 21:58:13 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = new Plyr('#player', {
         controls: [
             'play-large', // The large play button in the center
-
-
-           
-            
-            
-            
+            'play', // Play/pause playback
+            'progress', // The progress bar and scrubber for playback and buffering
+            'current-time', // The current time of playback
+            'duration', // The full duration of the media
             'mute', // Toggle mute
             'volume', // Volume control
             'captions', // Toggle captions
             'settings', // Settings menu
-            'quality',
+            'pip', // Picture-in-picture (currently Safari only)
             'airplay', // Airplay (currently Safari only)
-   
+            'fullscreen' // Toggle fullscreen
         ],
         captions: {active: false},
         speed: 	{selected: 1, options: [0.5, 1, 1.5, 2, 4]},
@@ -22,11 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
         hideControls: true,
         displayDuration: true,
         disableContextMenu: true,
-        clickToPlay: false,
-        autoplay: true,
+        clickToPlay: true,
         playsinline: false,
         tooltips: { controls: false, seek: true },
-        quality: {default: 576, options: [4320, 2880, 2160, 1440, 1080, 720, 576, 480, 360, 240]}
     });
 });
 
